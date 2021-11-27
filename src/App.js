8cf7a4efab7b3419574da7accb41015c1fe35ba3
ismail-anywhere <mohammed.abdul@anywhere.co>
2021-11-27 17:36:18 +0530

    add all files for redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, editText, increment } from './redux/actions'
 
 function App() {
+  const dispatch = useDispatch()
+  const counter = useSelector((state)=>state.counter)
+  const text = useSelector((state)=>state.text)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={()=>{
+        dispatch(increment())
+      }}>increment</button>
+      <button onClick={()=>{
+        dispatch(decrement())
+      }}>decrement</button>
+
+      <h2>{text}</h2>
+      <input onChange={(text)=>{
+        dispatch(editText(text.target.value))
+      }}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/redux/mainReducer.js
@@ -0,0 +1,20 @@
+
+
+const initialState = {
+    counter : 0,
+    text : ''
+}
+
+
+export const mainReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {...state , counter : state.counter+1}
+        case 'DECREMENT':
+            return {...state , counter : state.counter-1}
+        case 'EDITTEXT' : 
+            return {...state, text : action.text}
+        default:
+            return state
+    }
+}
